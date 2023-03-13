ee6dca7106f8a4f1e14905ffd47afeb5ed11ca5a
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2023-03-13 21:43:20 +0100

    setup

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { PhoneIcon } from '@heroicons/react/outline';
+
 import config from '../config/index.json';
 
 const MainHero = () => {
@@ -22,17 +24,18 @@ const MainHero = () => {
               href={mainHero.primaryAction.href}
               className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
             >
+              <PhoneIcon className="h-6 w-6 mr-2" />
               {mainHero.primaryAction.text}
             </a>
           </div>
-          <div className="mt-3 sm:mt-0 sm:ml-3">
+          {/* <div className="mt-3 sm:mt-0 sm:ml-3">
             <a
               href={mainHero.secondaryAction.href}
               className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-secondary bg-background hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
             >
               {mainHero.secondaryAction.text}
             </a>
-          </div>
+          </div> */}
         </div>
       </div>
     </main>
