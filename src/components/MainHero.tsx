29c5790caa22f33d61ccf13b4bcbe2c22499ac5a
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2023-03-13 21:46:17 +0100

    m

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -5,6 +5,7 @@ import { PhoneIcon } from '@heroicons/react/outline';
 import config from '../config/index.json';
 
 const MainHero = () => {
+  //
   const { mainHero } = config;
   return (
     <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
