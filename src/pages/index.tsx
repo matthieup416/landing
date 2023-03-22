bb8af42624851ee115572480075e13ba3f6cfe4c
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2023-03-22 08:25:33 +0100

    add getStaticProps fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,47 @@ import MainHeroImage from '../components/MainHeroImage';
 import Pricing from '../components/Pricing';
 import Product from '../components/Product';
 
-const App = () => {
+const URL = process.env.STRAPIBASEURL;
+console.log('STRAPIBASEURL', URL);
+export async function getStaticProps() {
+  const fetchParams = {
+    method: 'post',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `{
+        blogposts {
+          data {
+            attributes{
+              title
+              blogbody
+              description
+              slug
+              splash { 
+                data { 
+                  attributes { 
+                    url
+                  }
+                }
+              }
+            }
+          }
+        }
+    }
+      `,
+    }),
+  };
+
+  const res = await fetch(`${URL}/graphql`, fetchParams);
+  const data = await res.json();
+
+  return {
+    props: data,
+  };
+}
+const App = (data: any) => {
+  console.log('🤩data', data.data.blogposts.data);
   return (
     <div className={`bg-background grid gap-y-16 overflow-hidden`}>
       <div className={`relative bg-background`}>
